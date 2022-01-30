270dcf513c59a35d5d59d2ae67c26e1400b6a198
Akhilesh Muthusamy <akhileshm@outlook.in>
2022-01-30 15:39:56 -0500

    Test image to cam code

---
+++ image_to_cam.py
@@ -0,0 +1,21 @@
+import pyvirtualcam
+import cv2
+import sys
+
+
+def main(path):
+    frame = cv2.imread(path)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    width = frame.shape[1]
+    height = frame.shape[0]
+    cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
+    while True:
+        cam.send(frame)
+        cam.sleep_until_next_frame()
+    
+if __name__ == "__main__":
+    path = "media/1.jpg"
+    if len(sys.argv) == 2 :
+        path = sys.argv[1]
+    print(path)
+    main(path)
