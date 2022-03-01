a685d96baac99af010c57e379b32852ac6e98db6
Akhilesh Muthusamy <akhileshm@outlook.in>
2022-02-28 21:52:56 -0500

    Added option to mirror the image

--- image_to_cam.py
+++ image_to_cam.py
@@ -1,11 +1,14 @@
 import pyvirtualcam
 import cv2
 import sys
+import numpy as np
 
 
-def main(path):
+def main(path, mirror):
     frame = cv2.imread(path)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    if mirror:
+        frame = np.fliplr(frame)
     width = frame.shape[1]
     height = frame.shape[0]
     cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
@@ -14,8 +17,9 @@ def main(path):
         cam.sleep_until_next_frame()
     
 if __name__ == "__main__":
-    path = "media/1.jpg"
+    path = "media/images/1.jpeg"
+    mirror = True
     if len(sys.argv) == 2 :
         path = sys.argv[1]
     print(path)
-    main(path)
+    main(path, mirror)
